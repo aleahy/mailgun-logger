34a09c6380905691a560a5bd79a8d638d2321822
Alexander Leahy <alex@languagetub.com>
2021-05-16 14:13:28 +1000

    Added comments to make config clearer

--- config/mailgun-logger.php
+++ config/mailgun-logger.php
@@ -1,13 +1,17 @@
 <?php
 
 return [
-    //Put your mailgun signing secret in your env file
+    /**
+     * Put your mailgun signing secret in your env file
+     */
     'signing_secret' => env('MAILGUN_SIGNING_SECRET'),
 
-    // If you want to process a webhook, create a job to handle the type of event.
-    // Keys must be one of the event types found at
-    // https://documentation.mailgun.com/en/latest/api-events.html#event-types
+    /**
+     * If you want to process a webhook, create a job to handle the type of event.
+     * Keys must be one of the event types found at
+     * https://documentation.mailgun.com/en/latest/api-events.html#event-types
+     */
     'jobs' => [
-//       'failed' => HandleFailedJob::class
+        // 'failed' => HandleFailedJob::class
     ],
 ];
