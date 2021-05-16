cd67bb74ccb10f9fb330f54862b577f42e0e423d
Alexander Leahy <alex@languagetub.com>
2021-05-16 14:12:55 +1000

    Added getMime method to MailgunService

--- src/MailgunService.php
+++ src/MailgunService.php
@@ -8,11 +8,31 @@ use Illuminate\Support\Facades\Http;
 
 class MailgunService
 {
-    public static function get(string $url)
+    /**
+     * @param string $url
+     * @return array
+     *
+     * Gets the stored message at the provided Mailgun URL as an associative array.
+     */
+    public static function get(string $url): array
     {
         return Http::withBasicAuth('api', config('mailgun-logger.signing_secret'))
             ->get($url)->json();
+    }
 
-        return json_decode($response->body(), true);
+    /**
+     * @param string $url
+     * @return array
+     *
+     * Gets the stored message at the provided Mailgun URL as raw MIME.
+     */
+    public static function getMime(string $url): array
+    {
+        return Http::withBasicAuth('api', config('mailgun-logger.signing_secret'))
+            ->withHeaders([
+                'Accept' => 'message/rfc2822'
+            ])
+            ->get($url)->json();
+
     }
 }
